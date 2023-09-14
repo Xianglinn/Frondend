d7f8d92c8061115881bb07cd65beaae8ca3f8d28
Simon Wang <simonwang252@gmail.com>
2023-09-14 14:45:41 -0400

    first commit end of video 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyComponent } from './property/property.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavBarComponent,
+      PropertyComponent,
+      PropertyCardComponent
+   ],
   imports: [
     BrowserModule
   ],
